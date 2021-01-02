4f6e04344cfc46ba596de76d9a5934e9eef1adea
anurag <anuragkumarjsk13@gmail.com>
2021-01-02 16:21:14 +0530

    \before deployment

--- server.js
+++ server.js
@@ -37,6 +37,13 @@ app.get('/',function(req,res){
     res.send('Test successful');
 });
 
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(port,function(){
     console.log('server started on '+port);
 })
